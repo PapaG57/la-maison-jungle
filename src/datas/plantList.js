056e58aa489dd43331f79806be32473a1b721766
Florent GERARD <florentgerard@sfr.fr>
2022-08-04 15:00:44 +0200

    git commit "Correction fichier plantList "

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -9,40 +9,50 @@ export const plantList = [
     name: 'ficus lyrata',
     category: 'classique',
     id: '2ab',
+    isBestSale: true,
   },
   {
     name: 'pothos argenté',
     category: 'classique',
     id: '3sd',
+    isBestSale: true,
   },
   {
     name: 'yucca',
     category: 'classique',
     id: '4kk',
+    isBestSale: true,
   },
   {
     name: 'olivier',
     category: 'extérieur',
     id: '5pl',
+    isBestSale: true,
   },
   {
     name: 'géranium',
     category: 'extérieur',
     id: '6uo',
+    isBestSale: true,
   },
   {
     name: 'basilique',
     category: 'extérieur',
     id: '7ie',
+    isBestSale: true,
   },
   {
     name: 'aloe',
     category: 'plante grasse',
     id: '8fp',
+    isBestSale: true,
   },
   {
     name: 'succulente',
     category: 'plante grasse',
     id: '9vn',
+    isBestSale: true,
   },
 ];
+
+export default plantList;
